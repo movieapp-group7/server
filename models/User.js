f5cdb16c16835ab8166dc12ac771e552f669c363
PruttinuShane <156614994+PruttinuShane@users.noreply.github.com>
2024-11-18 11:07:57 +0200

    Revert "group query"
    This reverts commit 2fc85637ef791cea8e8e39306a165e2f6ab9d661.

--- models/User.js
+++ models/User.js
@@ -1,14 +1,10 @@
-export const insertUser = async (username, email, hashedPassword) => {
-  try {
-    const result = await pool.query(
-      'INSERT INTO account (username, email, password) VALUES ($1, $2, $3) RETURNING id, username, email',
-      [username, email, hashedPassword]
-    );
-    return result; // returns the inserted user details
-  } catch (error) {
-    if (error.code === '23505') { // Unique violation error code (for duplicate email)
-      throw new Error('Email already in use');
-    }
-    throw error; // rethrow other database-related errors
-  }
-};
+import {pool} from '../helpers/db.js'
+
+export const insertUser = async (username,email, hashedPassword) => {
+  return await pool.query('INSERT INTO account (username, email, password) VALUES ($1, $2, $3) returning *',
+    [username,email,hashedPassword])
+}
+
+export const selectUserByEmail = async (email) => {
+  return await pool.query('select * from account where email=$1',[email])
+}
